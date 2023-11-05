935db18ece5eee0e335c8639d4707a05ab33762e
Parsa Nasirikhah <Parsanasirikhah40@gmail.com>
2023-11-05 23:17:38 +0330

    Hanoi

---
+++ Hanoi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Hanoi
+    {
+        void hanoi(int Disk, char Shoro, char payan, char temp)
+        {
+            if (Disk == 1)
+            {
+                Console.WriteLine($"{Shoro} == {payan}");
+            }
+            else
+            {
+                hanoi(Disk - 1, Shoro, payan, temp);
+                Console.WriteLine($"{Shoro} -== {payan}");
+                hanoi(Disk - 1, temp, Shoro, payan);
+            }
+        }
+    }
+}
