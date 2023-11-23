b84d710e2032790ed3a975cc8d53f4a2b0f61653
ParsaNr <108646665+ParsaNr@users.noreply.github.com>
2023-11-24 01:08:36 +0330

    Stack

---
+++ Stack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Stack
+    {
+        private int count;
+
+        void Push(int[] items, int item)
+        {
+            if (count == items.Length)
+                throw new Exception("Queue Is Full");
+            items[count++] = item;
+        }
+
+        public int Pop(int[] items)
+        {
+            if (IsEmpty())
+                throw new Exception("Queue Is Empty");
+            return items[--count];
+        }
+
+        public int Peek(int[] items)
+        {
+            if (IsEmpty())
+            {
+                throw new Exception("Queue Is Empty");
+            }
+            return items[count - 1];
+        }
+
+
+        public Boolean IsEmpty()
+        {
+            if (count == 0)
+                return true;
+            return false;
+        }
+    }
+}
