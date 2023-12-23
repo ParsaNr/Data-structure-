f5ff1b53e8e12ec6b75af1bd1122c0be3847101b
ParsaNr <108646665+ParsaNr@users.noreply.github.com>
2023-12-24 00:39:02 +0330

    Sorts

---
+++ Sorts/QuickSort.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class QuickSort
+    {
+        //public void quicksort(int[] arr, int low, int high)
+        //{
+        //    int j;
+        //    if (low < high)
+        //    {
+        //        j = partition(arr, low, high + 1);
+        //        quicksort(arr, low, j - 1);
+        //        quicksort(arr, j + 1, high);
+        //    }
+        //}
+        public int[] partition(int[] arr, int low, int high)
+        {
+            int i, j, pvot;
+            pvot = arr[low];
+            i = low;
+            j = high;
+            while (i <= j)
+            {
+                while (arr[i] < pvot)
+                    i++;
+                while (arr[j] > pvot)
+                    j--;
+                if (i <= j)
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+            if (low < j)
+                partition(arr, low, j);
+            if (i < high)
+                partition(arr, i, high);
+            return arr;
+        }
+        public void printArray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i)
+                Console.Write(arr[i] + " ");
+
+            Console.Write("\n");
+        }
+    }
+}
