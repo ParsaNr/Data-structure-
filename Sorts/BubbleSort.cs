60bbbe0dd5877972c6f9c22972de4aed4a69edd6
ParsaNr <108646665+ParsaNr@users.noreply.github.com>
2023-12-24 00:15:54 +0330

    Add files via upload

---
+++ Sorts/BubbleSort.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class BubbleSort
+    {
+        public void Bubble(int[] a, int n)
+        {
+            for (int i = n; i < n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (a[j] < a[j + 1])
+                    {
+                        int temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
