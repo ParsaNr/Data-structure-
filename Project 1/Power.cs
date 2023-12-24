5668f49c96bdc00e3287565e237d9145c43a44ac
ParsaNr <108646665+ParsaNr@users.noreply.github.com>
2023-12-24 14:45:24 +0330

    Project 1

---
+++ Project 1/Power.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Power
+    {
+        int pow(int a, int n)
+        {
+            if (n == 0)
+                return 1;
+            return pow(a, n - 1) * a;
+        }
+    }
+}
