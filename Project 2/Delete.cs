23992dbb4964207eea4076561fade6ac55a24865
ParsaNr <108646665+ParsaNr@users.noreply.github.com>
2023-12-24 14:49:19 +0330

    Array

---
+++ Project 2/Delete.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Delete
+    {
+        int[] Remove(int[] a, int remove)
+        {
+            int c = 0;
+            foreach (int n in a)
+            {
+                if (n == remove)
+                {
+                    c++;
+                }
+            }
+
+            int[] update = new int[a.Length - c];
+            int index = 0;
+            foreach (int n in a)
+            {
+                if (n != remove)
+                {
+                    update[index] = n;
+                    index++;
+                }
+            }
+
+            return update;
+        }
+    }
+}
---
+++ Project 2/Spars.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Spars
+    {
+        void spars(int[][] a, int row, int col)
+        {
+            int k = 1;
+            int[,] spars = new int[100, 3];
+            for (int i = 0; i < row; i++)
+                for (int j = 0; j < col; j++)
+                {
+                    if (a[i][j] != 0)
+                    {
+                        spars[k, 0] = i;
+                        spars[k, 1] = i;
+                        spars[k, 2] = a[i][j];
+                        k++;
+                    }
+                }
+            spars[0, 0] = row;
+            spars[0, 1] = col;
+            spars[0, 2] = k - 1;
+        }
+    }
+}
